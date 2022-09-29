35841d2b502814257b68e73e2553e67176fea897
rafasanzvi <rafasv41@gmail.com>
2022-09-29 14:11:21 +0200

    Adding regular expressions and validating form

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ const messageField = document.querySelector("#mensaje")
 eventListeners()
 
 function eventListeners() {
+
     //When app start
     document.addEventListener("DOMContentLoaded", initApp)
 
@@ -29,18 +30,34 @@ function initApp() {
 
 function validateForm(e) {
     // console.log(e.target.value)
+    // console.log(e.target.type)
     if (e.target.value.length > 0) {
-        console.log("There is something in the input")
+        e.target.classList.remove("border", "border-red-500")
+        e.target.classList.add("border", "border-green-500")
     } else {
+        e.target.classList.remove("border", "border-green-500")
         e.target.classList.add("border", "border-red-500")
 
-        showError()
+        showError("You have to fill all fields")
+    }
+
+    if (e.target.type === "email") {
+        const regularExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        // const resultadoEmail = e.target.value.indexOf("@")
+        // console.log(resultadoEmail)
+        if (regularExpression.test(e.target.value)) {
+            console.log("Email valid")
+        } else {
+            e.target.classList.add("border", "border-red-500")
+
+            showError("The email is not valid")
+        }
     }
 }
 
-const showError = () => {
+const showError = (message) => {
     const errorMessage = document.createElement("p")
-    errorMessage.textContent = "You have to fill all fields"
+    errorMessage.textContent = message
     errorMessage.classList.add("border", "border-red-500", "background-red-100", "text-red-500", "p-3", "mt-5", "text-center", "error")
 
     const errors = document.querySelectorAll(".error")
